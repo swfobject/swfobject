0c4732d73b69ffce7f079bee05f9703c2cdf9b05
bobbyvandersluis <bobbyvandersluis@5cf17b04-402e-0410-8052-5f67ef7ba858>
2008-05-05 15:42:09 +0000

    This version is a work in progress and still needs regression testing.
    git-svn-id: https://swfobject.googlecode.com/svn/trunk@156 5cf17b04-402e-0410-8052-5f67ef7ba858

--- swfobject/src/swfobject.js
+++ swfobject/src/swfobject.js
@@ -1,4 +1,4 @@
-/*!	SWFObject v2.1 beta3 <http://code.google.com/p/swfobject/>
+/*!	SWFObject v2.1 beta4 <http://code.google.com/p/swfobject/>
 	Copyright (c) 2007 Geoff Stearns, Michael Williams, and Bobby van der Sluis
 	This software is released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
 */
@@ -346,80 +346,82 @@ var swfobject = function() {
 	*/
 	function createSWF(attObj, parObj, id) {
 		var r, el = getElementById(id);
-		if (typeof attObj.id == UNDEF) { // if no 'id' is defined for the object element, it will inherit the 'id' from the alternative content
-			attObj.id = id;
-		}
-		if (ua.ie && ua.win) { // IE, the object element and W3C DOM methods do not combine: fall back to outerHTML
-			var att = "";
-			for (var i in attObj) {
-				if (attObj[i] != Object.prototype[i]) { // Filter out prototype additions from other potential libraries, like Object.prototype.toJSONString = function() {}
-					if (i == "data") {
-						parObj.movie = attObj[i];
-					}
-					else if (i.toLowerCase() == "styleclass") { // 'class' is an ECMA4 reserved keyword
-						att += ' class="' + attObj[i] + '"';
-					}
-					else if (i != "classid") {
-						att += ' ' + i + '="' + attObj[i] + '"';
+		if (el) {
+			if (typeof attObj.id == UNDEF) { // if no 'id' is defined for the object element, it will inherit the 'id' from the alternative content
+				attObj.id = id;
+			}
+			if (ua.ie && ua.win) { // IE, the object element and W3C DOM methods do not combine: fall back to outerHTML
+				var att = "";
+				for (var i in attObj) {
+					if (attObj[i] != Object.prototype[i]) { // Filter out prototype additions from other potential libraries, like Object.prototype.toJSONString = function() {}
+						if (i.toLowerCase() == "data") {
+							parObj.movie = attObj[i];
+						}
+						else if (i.toLowerCase() == "styleclass") { // 'class' is an ECMA4 reserved keyword
+							att += ' class="' + attObj[i] + '"';
+						}
+						else if (i.toLowerCase() != "classid") {
+							att += ' ' + i + '="' + attObj[i] + '"';
+						}
 					}
 				}
-			}
-			var par = "";
-			for (var j in parObj) {
-				if (parObj[j] != Object.prototype[j]) { // Filter out prototype additions from other potential libraries
-					par += '<param name="' + j + '" value="' + parObj[j] + '" />';
-				}
-			}
-			el.outerHTML = '<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"' + att + '>' + par + '</object>';
-			objIdArr[objIdArr.length] = attObj.id; // Stored to fix object 'leaks' on unload (dynamic publishing only)
-			r = getElementById(attObj.id);	
-		}
-		else if (ua.webkit && ua.webkit < 312) { // Older webkit engines ignore the object element's nested param elements: fall back to the proprietary embed element
-			var e = createElement("embed");
-			e.setAttribute("type", FLASH_MIME_TYPE);
-			for (var k in attObj) {
-				if (attObj[k] != Object.prototype[k]) { // Filter out prototype additions from other potential libraries
-					if (k == "data") {
-						e.setAttribute("src", attObj[k]);
-					}
-					else if (k.toLowerCase() == "styleclass") { // 'class' is an ECMA4 reserved keyword
-						e.setAttribute("class", attObj[k]);
-					}
-					else if (k != "classid") { // Filter out IE specific attribute
-						e.setAttribute(k, attObj[k]);
+				var par = "";
+				for (var j in parObj) {
+					if (parObj[j] != Object.prototype[j]) { // Filter out prototype additions from other potential libraries
+						par += '<param name="' + j + '" value="' + parObj[j] + '" />';
 					}
 				}
+				el.outerHTML = '<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"' + att + '>' + par + '</object>';
+				objIdArr[objIdArr.length] = attObj.id; // Stored to fix object 'leaks' on unload (dynamic publishing only)
+				r = getElementById(attObj.id);	
 			}
-			for (var l in parObj) {
-				if (parObj[l] != Object.prototype[l]) { // Filter out prototype additions from other potential libraries
-					if (l != "movie") { // Filter out IE specific param element
-						e.setAttribute(l, parObj[l]);
+			else if (ua.webkit && ua.webkit < 312) { // Older webkit engines ignore the object element's nested param elements: fall back to the proprietary embed element
+				var e = createElement("embed");
+				e.setAttribute("type", FLASH_MIME_TYPE);
+				for (var k in attObj) {
+					if (attObj[k] != Object.prototype[k]) { // Filter out prototype additions from other potential libraries
+						if (k.toLowerCase() == "data") {
+							e.setAttribute("src", attObj[k]);
+						}
+						else if (k.toLowerCase() == "styleclass") { // 'class' is an ECMA4 reserved keyword
+							e.setAttribute("class", attObj[k]);
+						}
+						else if (k.toLowerCase() != "classid") { // Filter out IE specific attribute
+							e.setAttribute(k, attObj[k]);
+						}
 					}
 				}
-			}
-			el.parentNode.replaceChild(e, el);
-			r = e;
-		}
-		else { // Well-behaving browsers
-			var o = createElement(OBJECT);
-			o.setAttribute("type", FLASH_MIME_TYPE);
-			for (var m in attObj) {
-				if (attObj[m] != Object.prototype[m]) { // Filter out prototype additions from other potential libraries
-					if (m.toLowerCase() == "styleclass") { // 'class' is an ECMA4 reserved keyword
-						o.setAttribute("class", attObj[m]);
-					}
-					else if (m != "classid") { // Filter out IE specific attribute
-						o.setAttribute(m, attObj[m]);
+				for (var l in parObj) {
+					if (parObj[l] != Object.prototype[l]) { // Filter out prototype additions from other potential libraries
+						if (l.toLowerCase() != "movie") { // Filter out IE specific param element
+							e.setAttribute(l, parObj[l]);
+						}
 					}
 				}
+				el.parentNode.replaceChild(e, el);
+				r = e;
 			}
-			for (var n in parObj) {
-				if (parObj[n] != Object.prototype[n] && n != "movie") { // Filter out prototype additions from other potential libraries and IE specific param element
-					createObjParam(o, n, parObj[n]);
+			else { // Well-behaving browsers
+				var o = createElement(OBJECT);
+				o.setAttribute("type", FLASH_MIME_TYPE);
+				for (var m in attObj) {
+					if (attObj[m] != Object.prototype[m]) { // Filter out prototype additions from other potential libraries
+						if (m.toLowerCase() == "styleclass") { // 'class' is an ECMA4 reserved keyword
+							o.setAttribute("class", attObj[m]);
+						}
+						else if (m.toLowerCase() != "classid") { // Filter out IE specific attribute
+							o.setAttribute(m, attObj[m]);
+						}
+					}
 				}
+				for (var n in parObj) {
+					if (parObj[n] != Object.prototype[n] && n != "movie") { // Filter out prototype additions from other potential libraries and IE specific param element
+						createObjParam(o, n, parObj[n]);
+					}
+				}
+				el.parentNode.replaceChild(o, el);
+				r = o;
 			}
-			el.parentNode.replaceChild(o, el);
-			r = o;
 		}
 		return r;
 	}
@@ -483,6 +485,8 @@ var swfobject = function() {
 		listenersArr[listenersArr.length] = [target, eventType, fn];
 	}
 	
+	/* Flash Player and SWF content version matching
+	*/
 	function hasPlayerVersion(rv) {
 		var pv = ua.pv, v = rv.split(".");
 		v[0] = parseInt(v[0], 10);
@@ -515,7 +519,7 @@ var swfobject = function() {
 	
 	function setVisibility(id, isVisible) {
 		var v = isVisible ? "visible" : "hidden";
-		if (isDomLoaded) {
+		if (isDomLoaded && getElementById(id)) {
 			getElementById(id).style.visibility = v;
 		}
 		else {
@@ -570,7 +574,7 @@ var swfobject = function() {
 		
 		getObjectById: function(objectIdStr) {
 			var r = null;
-			if (ua.w3cdom && isDomLoaded) {
+			if (ua.w3cdom) {
 				var o = getElementById(objectIdStr);
 				if (o) {
 					var n = o.getElementsByTagName(OBJECT)[0];
@@ -638,7 +642,7 @@ var swfobject = function() {
 		hasFlashPlayerVersion:hasPlayerVersion,
 		
 		createSWF: function(attObj, parObj, replaceElemIdStr) {
-			if (ua.w3cdom && isDomLoaded) {
+			if (ua.w3cdom) {
 				return createSWF(attObj, parObj, replaceElemIdStr);
 			}
 			else {
@@ -647,7 +651,7 @@ var swfobject = function() {
 		},
 		
 		removeSWF: function(objElemIdStr) {
-			if (ua.w3cdom && isDomLoaded) {
+			if (ua.w3cdom) {
 				removeSWF(objElemIdStr);
 			}
 		},
