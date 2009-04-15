805d33842cc46dcb853a42c4c50661fd3591e1ab
bobbyvandersluis <bobbyvandersluis@5cf17b04-402e-0410-8052-5f67ef7ba858>
2009-04-15 15:49:40 +0000

    swfobject 2.2 alpha12 bug fixes
    git-svn-id: https://swfobject.googlecode.com/svn/trunk@323 5cf17b04-402e-0410-8052-5f67ef7ba858

--- swfobject/src/swfobject.js
+++ swfobject/src/swfobject.js
@@ -1,4 +1,4 @@
-/*!	SWFObject v2.2 alpha11 <http://code.google.com/p/swfobject/> 
+/*!	SWFObject v2.2 alpha12 <http://code.google.com/p/swfobject/> 
 	is released under the MIT License <http://www.opensource.org/licenses/mit-license.php> 
 */
 
@@ -42,13 +42,14 @@ var swfobject = function() {
 			windows = p ? /win/.test(p) : /win/.test(u),
 			mac = p ? /mac/.test(p) : /mac/.test(u),
 			webkit = /webkit/.test(u) ? parseFloat(u.replace(/^.*webkit\/(\d+(\.\d+)?).*$/, "$1")) : false, // returns either the webkit version or false if not webkit
-			ie = false, // the actual feature test will happen later on
+			ie = !+"\v1", // feature detection based on Andrea Giammarchi's solution: http://webreflection.blogspot.com/2009/01/32-bytes-to-know-if-your-browser-is-ie.html
 			playerVersion = [0,0,0],
 			d = null;
 		if (typeof nav.plugins != UNDEF && typeof nav.plugins[SHOCKWAVE_FLASH] == OBJECT) {
 			d = nav.plugins[SHOCKWAVE_FLASH].description;
 			if (d && !(typeof nav.mimeTypes != UNDEF && nav.mimeTypes[FLASH_MIME_TYPE] && !nav.mimeTypes[FLASH_MIME_TYPE].enabledPlugin)) { // navigator.mimeTypes["application/x-shockwave-flash"].enabledPlugin indicates whether plug-ins are enabled or disabled in Safari 3+
 				plugin = true;
+				ie = false; // cascaded feature detection for Internet Explorer
 				d = d.replace(/^.*\s+(\S+\s+\S+$)/, "$1");
 				playerVersion[0] = parseInt(d.replace(/^(.*)\..*$/, "$1"), 10);
 				playerVersion[1] = parseInt(d.replace(/^.*\.(.*)\s.*$/, "$1"), 10);
@@ -61,7 +62,7 @@ var swfobject = function() {
 				if (a) { // a will return null when ActiveX is disabled
 					d = a.GetVariable("$version");
 					if (d) {
-						ie = true;
+						ie = true; // cascaded feature detection for Internet Explorer
 						d = d.split(" ")[1].split(",");
 						playerVersion = [parseInt(d[0], 10), parseInt(d[1], 10), parseInt(d[2], 10)];
 					}
@@ -211,11 +212,11 @@ var swfobject = function() {
 				}
 				b.removeChild(o);
 				t = null;
-				setTimeout(matchVersions, 10); // give object element time to initialize in Firefox
+				setTimeout(matchVersions, 100); // give object element time to initialize in Firefox
 			})();
 		}
 		else {
-			setTimeout(matchVersions, 10);
+			setTimeout(matchVersions, 100);
 		}
 	}
 	
@@ -282,12 +283,14 @@ var swfobject = function() {
 		var r = null;
 		var o = getElementById(objectIdStr);
 		if (o && o.nodeName == "OBJECT") {
-			var n = o.getElementsByTagName(OBJECT)[0];
-			if (!n || (n && typeof o.SetVariable != UNDEF)) {
+			if (typeof o.SetVariable != UNDEF) {
 				r = o;
 			}
-			else if (typeof n.SetVariable != UNDEF) {
-				r = n;
+			else {
+				var n = o.getElementsByTagName(OBJECT)[0];
+				if (n && typeof n.SetVariable != UNDEF) {
+					r = n;
+				}
 			}
 		}
 		return r;
