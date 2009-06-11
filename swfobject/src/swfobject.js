9e0104326a8913918f2170f15a69d60624c4bf68
bobbyvandersluis <bobbyvandersluis@5cf17b04-402e-0410-8052-5f67ef7ba858>
2009-06-11 13:13:59 +0000

    SWFObject 2.2 release version
    git-svn-id: https://swfobject.googlecode.com/svn/trunk@374 5cf17b04-402e-0410-8052-5f67ef7ba858

--- swfobject/src/swfobject.js
+++ swfobject/src/swfobject.js
@@ -1,4 +1,4 @@
-/*!	SWFObject v2.2 beta1 <http://code.google.com/p/swfobject/> 
+/*!	SWFObject v2.2 <http://code.google.com/p/swfobject/> 
 	is released under the MIT License <http://www.opensource.org/licenses/mit-license.php> 
 */
 
