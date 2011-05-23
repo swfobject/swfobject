e48cf34157887141f3f7ee67606131a9bc41909f
Philip Hutchison <philip@pipwerks.com>
2011-05-22 22:13:02 -0700

    Issue 451: adding Object.prototype.function makes generated flashvars invalid
    http://code.google.com/p/swfobject/issues/detail?id=451
    
    Adding a simple obj.hasOwnProperty(prop) check fixes the problem.
    
    Signed-off-by: Philip Hutchison <philip@pipwerks.com>

--- swfobject/src/swfobject.js
+++ swfobject/src/swfobject.js
@@ -694,11 +694,13 @@ var swfobject = function() {
 					}
 					if (flashvarsObj && typeof flashvarsObj === OBJECT) {
 						for (var k in flashvarsObj) { // copy object to avoid the use of references, because web authors often reuse flashvarsObj for multiple SWFs
-							if (typeof par.flashvars != UNDEF) {
-								par.flashvars += "&" + k + "=" + flashvarsObj[k];
-							}
-							else {
-								par.flashvars = k + "=" + flashvarsObj[k];
+							if(flashvarsObj.hasOwnProperty(k)){
+								if (typeof par.flashvars != UNDEF) {
+									par.flashvars += "&" + k + "=" + flashvarsObj[k];
+								}
+								else {
+									par.flashvars = k + "=" + flashvarsObj[k];
+								}
 							}
 						}
 					}
