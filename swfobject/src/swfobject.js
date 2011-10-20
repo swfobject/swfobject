e61df5156ff12052285d8dad65db71d450c40b9d
Philip Hutchison <philip@pipwerks.com>
2011-10-19 22:34:28 -0700

    Revert "Replaced all instances of arguments.callee with named functions"
    This reverts commit 52d1063c9d1a2d02bd3535aa81442bf037bc61ab.

--- swfobject/src/swfobject.js
+++ swfobject/src/swfobject.js
@@ -88,20 +88,20 @@ var swfobject = function() {
 				doc.addEventListener("DOMContentLoaded", callDomLoadFunctions, false);
 			}		
 			if (ua.ie && ua.win) {
-				doc.attachEvent(ON_READY_STATE_CHANGE, function detachDOMContentLoaded() {
+				doc.attachEvent(ON_READY_STATE_CHANGE, function() {
 					if (doc.readyState == "complete") {
-						doc.detachEvent(ON_READY_STATE_CHANGE, detachDOMContentLoaded);
+						doc.detachEvent(ON_READY_STATE_CHANGE, arguments.callee);
 						callDomLoadFunctions();
 					}
 				});
 				if (win == top) { // if not inside an iframe
-					(function ieDomLoadCheck(){
+					(function(){
 						if (isDomLoaded) { return; }
 						try {
 							doc.documentElement.doScroll("left");
 						}
 						catch(e) {
-							setTimeout(ieDomLoadCheck, 0);
+							setTimeout(arguments.callee, 0);
 							return;
 						}
 						callDomLoadFunctions();
@@ -109,10 +109,10 @@ var swfobject = function() {
 				}
 			}
 			if (ua.wk) {
-				(function webkitDomLoadCheck(){
+				(function(){
 					if (isDomLoaded) { return; }
 					if (!/loaded|complete/.test(doc.readyState)) {
-						setTimeout(webkitDomLoadCheck, 0);
+						setTimeout(arguments.callee, 0);
 						return;
 					}
 					callDomLoadFunctions();
@@ -197,7 +197,7 @@ var swfobject = function() {
 		var t = b.appendChild(o);
 		if (t) {
 			var counter = 0;
-			(function checkVersion(){
+			(function(){
 				if (typeof t.GetVariable != UNDEF) {
 					var d = t.GetVariable("$version");
 					if (d) {
@@ -207,7 +207,7 @@ var swfobject = function() {
 				}
 				else if (counter < 10) {
 					counter++;
-					setTimeout(checkVersion, 10);
+					setTimeout(arguments.callee, 10);
 					return;
 				}
 				b.removeChild(o);
@@ -306,16 +306,6 @@ var swfobject = function() {
 		return !isExpressInstallActive && hasPlayerVersion("6.0.65") && (ua.win || ua.mac) && !(ua.wk && ua.wk < 312);
 	}
 	
-	/* Utility function pulled out of showExpressInstall and displayAltContent for DRY reasons */
-	function removeChildObj(obj){
-		if (obj.readyState == 4) {
-			obj.parentNode.removeChild(obj);
-		}
-		else {
-			setTimeout(removeChildObj, 10);
-		}
-	}
-
 	/* Show the Adobe Express Install dialog
 		- Reference: http://www.adobe.com/cfusion/knowledgebase/index.cfm?id=6a253b75
 	*/
@@ -353,12 +343,19 @@ var swfobject = function() {
 				newObj.setAttribute("id", replaceElemIdStr);
 				obj.parentNode.insertBefore(newObj, obj); // insert placeholder div that will be replaced by the object element that loads expressinstall.swf
 				obj.style.display = "none";
-				removeChildObj(obj);
+				(function(){
+					if (obj.readyState == 4) {
+						obj.parentNode.removeChild(obj);
+					}
+					else {
+						setTimeout(arguments.callee, 10);
+					}
+				})();
 			}
 			createSWF(att, par, replaceElemIdStr);
 		}
 	}
-		
+	
 	/* Functions to abstract and display alternative content
 	*/
 	function displayAltContent(obj) {
@@ -369,7 +366,14 @@ var swfobject = function() {
 			obj.parentNode.insertBefore(el, obj); // insert placeholder div that will be replaced by the alternative content
 			el.parentNode.replaceChild(abstractAltContent(obj), el);
 			obj.style.display = "none";
-			removeChildObj(obj);
+			(function(){
+				if (obj.readyState == 4) {
+					obj.parentNode.removeChild(obj);
+				}
+				else {
+					setTimeout(arguments.callee, 10);
+				}
+			})();
 		}
 		else {
 			obj.parentNode.replaceChild(abstractAltContent(obj), obj);
@@ -472,12 +476,12 @@ var swfobject = function() {
 		if (obj && obj.nodeName == "OBJECT") {
 			if (ua.ie && ua.win) {
 				obj.style.display = "none";
-				(function checkReadyState(){
+				(function(){
 					if (obj.readyState == 4) {
 						removeObjectInIE(id);
 					}
 					else {
-						setTimeout(checkReadyState, 10);
+						setTimeout(arguments.callee, 10);
 					}
 				})();
 			}
